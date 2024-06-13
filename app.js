db6000fab93a78e9dc20682b03f014eeedec0484
Qasimmemon <qasimarif0321@gmail.com>
2024-06-13 09:06:10 +0500

    Update app.js

--- app.js
+++ app.js
@@ -1,86 +1,78 @@
  var login_container = document.getElementById("login_container");
-var home_container = document.getElementById("home_container");
-var email = document.getElementById("email");
-var user_email = document.getElementById("user_email");
-var password = document.getElementById("password");
-var todo_input = document.getElementById("todo_input");
-var Fullname = document.getElementById('Fullname')
+        var home_container = document.getElementById("home_container");
+        var email = document.getElementById("email");
+        var user_email = document.getElementById("user_email");
+        var password = document.getElementById("password");
+        var todo_input = document.getElementById("todo_input");
 
-function checkIsUserLogin() {
-  var email = localStorage.getItem("email");
-  if (email) {
-    login_container.style.display = "none";
-    home_container.style.display = "block";
-    user_email.innerText = email;
-showTodos();
+        function checkIsUserLogin() {
+            var email = localStorage.getItem("email");
+            if (email) {
+                login_container.style.display = "none";
+                home_container.style.display = "block";
+                user_email.innerText = email;
+                showTodos();
+            } else {
+                login_container.style.display = "block";
+                home_container.style.display = "none";
+            }
+        }
 
-  } else {
-    login_container.style.display = "block";
-    home_container.style.display = "none";
-  }
-}
+        checkIsUserLogin();
 
-checkIsUserLogin();
+        function loginUser() {
+            if (!email.value || !password.value) return alert("Please add all details");
+            localStorage.setItem("email", email.value);
+            localStorage.setItem("password", password.value);
+            checkIsUserLogin();
+            email.value = "";
+            password.value = "";
+        }
 
-function loginUser() {
-  if (!email.value || !password.value) return alert("Please add all detaille");
-  localStorage.setItem("email", email.value);
-  localStorage.setItem("password", password.value);
-  checkIsUserLogin();
+        function logout() {
+            localStorage.removeItem("email");
+            checkIsUserLogin();
+        }
 
-  email.value = "";
-  password.value = "";
-}
-function logout() {
-  localStorage.removeItem("email");
-  checkIsUserLogin();
-}
+        function addTodo() {
+            if (!todo_input.value.trim()) return alert("Fill all instructions");
+            var email = localStorage.getItem("email");
+            var obj = {
+                email: email,
+                todo: todo_input.value.trim()
+            };
+            var todos = localStorage.getItem("todos");
+            if (todos) {
+                todos = JSON.parse(todos);
+                todos.push(obj);
+                localStorage.setItem("todos", JSON.stringify(todos));
+            } else {
+                todos = [obj];
+                localStorage.setItem("todos", JSON.stringify(todos));
+            }
+            showTodos();
+        }
 
-function addTodo() {
-  if (!todo_input.value?.trim()) return alert("Fill all instruction");
+        function showTodos() {
+            var todos = localStorage.getItem("todos");
+            var list = document.getElementById("list");
+            var email = localStorage.getItem("email");
+            if (todos) {
+                todos = JSON.parse(todos);
+                list.innerHTML = "";
+                todos.forEach(function (data) {
+                    if (data.email === email) {
+                        var li = document.createElement("li");
+                        li.innerHTML = `${data.todo} <button class="btn1" onclick="del(this)">Delete</button>`;
+                        list.appendChild(li);
+                    }
+                });
+            }
+        }
 
-  var email = localStorage.getItem("email");
-
-  var obj = {
-    email: email,
-    todo: todo_input.value.trim(),
-  };
-  var todos = localStorage.getItem("todos");
-  console.log("Local Storage se get kye=>", todos);
-  if (todos) {
-    todos = JSON.parse(todos);
-    todos.push(obj);
-    localStorage.setItem("todos", JSON.stringify(todos));
-  } else {
-    todos = [obj];
-    console.log("Todos null ho to naya banado=>", todos);
-    localStorage.setItem("todos", JSON.stringify(todos));
-  }
-
-  showTodos();
-}
-
-function showTodos() {
-  var todos = localStorage.getItem("todos");
-  var list = document.getElementById("list");
-  var email = localStorage.getItem("email");
-  if (todos) {
-    todos = JSON.parse(todos);
-    list.innerHTML = "";
-    todos.forEach(function (data, index) {
-      if (data.email === email) {
-        var li = `<li>${data.todo} <button class="btn1" onclick="del(li)">Delete</button><button  <button class="btn2" onclick="edit(li)">Edit</button></li>`;
-        list.innerHTML += li;
-      }
-    });
-  }
-}
-showTodos();
-// function del(li) {
-//   li.style.display = 'none';
-// }
-
-function del(button) {
+        function del(button) {
             var li = button.parentElement;
             li.parentElement.removeChild(li);
         }
+
+        showTodos();
